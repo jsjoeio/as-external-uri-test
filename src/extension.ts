7b997265bd597d2899f9976d200f7152ccb79131
Joe Previte <jjprevite@gmail.com>
2022-10-07 11:04:03 -0700

    refactor: ask for input

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,15 @@
 import * as vscode from "vscode";
-import { handleTestCase, TEST_CASES, ValidTestCase } from "./tests";
-
-function buildComand(testCase: ValidTestCase) {
-  let command = vscode.commands.registerCommand(
-    `as-external-uri.${testCase}`,
-    async () => {
-      await handleTestCase(testCase);
-    }
-  );
-  return command;
-}
+import { askTestCase } from "./tests";
 
 export function activate(context: vscode.ExtensionContext) {
-  let commandsToBuild = Object.keys(TEST_CASES) as ValidTestCase[];
-  const disposables = commandsToBuild.map((command) => buildComand(command));
-  disposables.forEach((disposable) => context.subscriptions.push(disposable));
+  const disposable = vscode.commands.registerCommand(
+    `as-external-uri.askTestCase`,
+    async () => {
+      await askTestCase();
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
--- src/tests.ts
+++ src/tests.ts
@@ -1,33 +1,16 @@
 import * as vscode from "vscode";
 
-export const TEST_CASES = {
-  localhostPort: {
-    input: "http://localhost:3000",
-    expected: "<host>/proxy/3000",
-  },
-  localhostNoPort: {
-    input: "http://localhost",
-    expected: "<host>/proxy/80",
-  },
-  localhostNoPortHttps: {
-    input: "https://localhost",
-    expected: "<host>/proxy/443",
-  },
-  noLocalhost: {
-    input: "https://google.com",
-    expected: "https://google.com",
-  },
-};
+export async function askTestCase() {
+  const input = await vscode.window.showInputBox({
+    prompt: "URL to pass through to asExternalUri",
+  });
 
-export type ValidTestCase = keyof typeof TEST_CASES;
-
-export async function handleTestCase(testCase: ValidTestCase) {
-  const externalUri = await vscode.env.asExternalUri(
-    vscode.Uri.parse(TEST_CASES[testCase].input)
-  );
-  vscode.window.showInformationMessage(`
-input: ${TEST_CASES[testCase].input} 
-output: ${externalUri}
-expected: ${TEST_CASES[testCase].expected} 
-  `);
+  if (input) {
+    const output = await vscode.env.asExternalUri(vscode.Uri.parse(input));
+    vscode.window.showInformationMessage(`input: ${input} output: ${output}`);
+  } else {
+    vscode.window.showErrorMessage(
+      `Failed to run test case. No input provided.`
+    );
+  }
 }
